6f6a4d67f07731bcd9cd00ccf6b7cbd28c0ea36e
Miroslav Tzarev <mtzarev@gmail.com>
2025-01-28 12:51:32 +0200

    useEffect for code to catch width and height

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import MyComponent from "./components/MyComponent"
-
+import WidthHeight from "./components/WidthHeight"
 function App() {
   
 
   return (
    <div>
+    <WidthHeight></WidthHeight>
+    <hr />
     <MyComponent></MyComponent>
    </div>
   )
